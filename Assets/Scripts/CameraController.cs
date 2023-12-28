91b61c8518ca840a1d9bc5d0ca58e46d28e8645f
13189803173@163.com <13189803173@163.com>
2023-12-29 02:47:51 +0800

    创建了场景 实现了相机跟随人物的上下左右移动视角，增加了焦点，相机倒转的功能

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform followTraget;
+    [SerializeField] float distance = 5f;
+    [SerializeField] float rotatespeed = 2f;
+
+    [SerializeField] float minVerticalangle = -45f;
+    [SerializeField] float maxVerticalangle = 45f;
+    [SerializeField] Vector2 farmingOffset ;
+
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+
+    float rotationY;
+    float rotationX;
+
+    float invertXval;
+    float invertYval;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+    private void Update()
+    {
+        invertXval = (invertX) ? -1 : 1;//倒转相机的判断
+        invertYval = (invertY) ? -1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y")* invertYval * rotatespeed;//鼠标的上下移动决定镜头绕X轴旋转
+        rotationX = Mathf.Clamp(rotationX, minVerticalangle, maxVerticalangle);//限制镜头上下移动的角度
+       
+        rotationY += Input.GetAxis("Mouse X")* invertXval * rotatespeed;//鼠标的左右移动决定镜头绕Y轴旋转
+        
+        var targetrotation = Quaternion.Euler(rotationX, rotationY, 0);
+        
+        var focuposition = transform.position + new Vector3( farmingOffset.x, farmingOffset.y);//设置一个镜头焦点，镜头应该照着人物的胸部以上位置
+        
+        transform.position = focuposition - targetrotation * new Vector3(0, 0, distance);
+        transform.rotation = targetrotation;
+    }
+
+}
