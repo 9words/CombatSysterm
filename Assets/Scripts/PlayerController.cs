72e0581f45202a681367c1be44ce4e3389e9f518
13189803173@163.com <13189803173@163.com>
2023-12-30 00:54:33 +0800

    1、增加了player的动画locomotion动画混合树 用移动的量来控制人物的动画的变化，记得要做停下的丝滑过度，设置阻尼值即可

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,15 +10,17 @@ public class PlayerController : MonoBehaviour
     float rotationSpeed = 500f;
     Quaternion targetRotation;
     CameraController cameraController;
+    Animator animator;
     private void Awake()
     {
         cameraController = Camera.main.GetComponent<CameraController>();
+        animator=GetComponent<Animator>();
     }
     private void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        float moveAmount =Mathf.Abs( h) + Mathf.Abs(v);//人物移动的量
+        float moveAmount =Mathf.Clamp01(Mathf.Abs( h) + Mathf.Abs(v));//人物移动的量做为动画混合树的移动量，需要限制在0-1之间
 
         var  moveInput= (new Vector3(h, 0, v)).normalized;//向量归一化，不让对角线的移动比单一方向的移动快
         var movedir = cameraController.PlanarRotation * moveInput;
@@ -28,6 +30,8 @@ public class PlayerController : MonoBehaviour
             targetRotation = Quaternion.LookRotation(movedir);//人物面向移动的方向
         }
 
-        transform.rotation=Quaternion.RotateTowards(transform.rotation,targetRotation,rotationSpeed*Time.deltaTime);//使得人物的旋转变成丝滑旋转不是瞬间转向
+        transform.rotation=Quaternion.RotateTowards(transform.rotation,targetRotation,rotationSpeed
+            *Time.deltaTime);//使得人物的旋转变成丝滑旋转不是瞬间转向
+        animator.SetFloat("moveAmount",moveAmount,0.2f,Time.deltaTime);//第三个参数是阻尼值，使得停下不是瞬间停下的，可以平滑过度
     }
 }
