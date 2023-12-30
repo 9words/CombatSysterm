7f54e1d99713d2976bab959e480754dd779ac30f
13189803173@163.com <13189803173@163.com>
2023-12-30 16:22:32 +0800

    增加敌人及其碰撞，增加敌人的受击反应动画 存在bug，就是撞到碰撞盒就受到击了，并没有攻击他，下一步解决这个问题

--- Assets/Scripts/CombatSystem/MeeleFighter.cs
+++ Assets/Scripts/CombatSystem/MeeleFighter.cs
@@ -31,4 +31,25 @@ public class MeeleFighter : MonoBehaviour
         InAction = false;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="HitBox"&&!InAction)
+        {
+            StartCoroutine("PlayHitReaction");
+        }
+    }
+
+    IEnumerator PlayHitReaction()
+    {
+        InAction = true;
+        animator.CrossFade("SwordImpact", 0.2f);//动画交叉淡出淡入，这里的0.2f不是0.2秒，在交叉淡出淡入这里0.2f值得是用当前的动画的20%时长来过度到别的动画，
+                                          //并且这个函数是在当前帧之后启动的
+        yield return null;//因为CrossFade函数是在当前帧之后启动的所以这里下面的执行需要等一帧
+
+        var animState = animator.GetNextAnimatorStateInfo(1);//获取下一个要播放的动画状态信息
+
+        yield return new WaitForSeconds(animState.length);//等到攻击的动画播放完
+        InAction = false;
+    }
+
 }
